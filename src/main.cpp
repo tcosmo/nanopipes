2afd06311e773c10f134f6e3b6ac5cb5df1ed7c7
Cosmo <tristan.sterin@gmail.com>
2020-05-31 20:21:39 +0100

    Col mode working!

--- src/main.cpp
+++ src/main.cpp
@@ -9,27 +9,26 @@ int main(int argc, char** argv)
 
     World world;
 
-    if( input_parser.cmdOptionExists("-l") ) {
+    if( input_parser.cmdOptionExists("-line") ) {
         world.set_mode(LINE_MODE);
     }
 
-    const std::string &mode_line_input = input_parser.getCmdOption("-l");
+    const std::string &mode_line_input = input_parser.getCmdOption("-line");
     if (!mode_line_input.empty()) {
         world.set_initial_cells(mode_line_input);
         world.bootstrap_Collatz();
     }
 
-    if( input_parser.cmdOptionExists("-c") ) {
+    if( input_parser.cmdOptionExists("-col") ) {
         world.set_mode(COL_MODE);
     }
 
-    const std::string &mode_col = input_parser.getCmdOption("-c");
+    const std::string &mode_col = input_parser.getCmdOption("-col");
     if (!mode_col.empty()) {
         world.set_initial_cells(mode_col);
         world.bootstrap_Collatz();
     }
 
     GraphicEngine engine(world, 800*1.5,600*1.5);
-
     engine.run();
 }
